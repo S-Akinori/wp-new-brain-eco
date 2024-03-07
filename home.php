745a81a504384fa10c4c1a5579c3b4d3fa8d829b
akinori <aki10.working@gmail.com>
2024-03-07 17:20:38 +0900

    fix

--- home.php
+++ home.php
@@ -21,4 +21,47 @@
       <div class="text-xl font-bold text-center">小型太陽光パネルとの併用で最大2/3まで削減できます！</div>
     </div>
   </section>
+  <section class="c-section text-center">
+    <h2 class="c-title">2024年1月から電気代がさらに値上がり！</h2>
+      <p>
+        <span class="c-mark">毎年値上がりする電気代</span> <br>
+        今年１月からさらに値上げされたのはご存じですか！？
+      </p>
+      <div class="mb-8">
+        <img src="<?= get_template_directory_uri();?>/assets/images/news.png" alt="">
+      </div>
+      <p>しかし私は、<br>
+      国が推奨するある方法で<br>
+      <span class="c-mark">電気代を最大3分の2削減</span>することができました！</p>
+      <p>それは、<span class="c-mark">蓄電池</span>を設置することです！</p>
+      <img src="<?= get_template_directory_uri();?>/assets/images/banner.png" alt="">
+  </section>
+  <section class="c-section">
+    <h2 class="c-title">蓄電池を設置して電気代を安く！</h2>
+    <p>ご存知ですか、電気代は<span class="c-mark">昼間と夜間では料金が違います。</span></p>
+    <p>
+      蓄電池を設置することで電気代が安い夜間に電気を溜めて、その溜まった電気を昼間に使うことでかなりの電気代を削減できます！
+    </p>
+    <div class="text-center mb-8">
+      <img class="mx-auto" src="<?= get_template_directory_uri();?>/assets/images/peakcut.png" alt="">
+    </div>
+    <p>太陽光パネルと蓄電池を両方設置することで「溜めて使う」ことができ、電気代を最大で3分の2削減が可能です！</p>
+    <p>蓄電池は災害時や停電時だけではなく、高騰する電気代の最大の味方なのです。</p>
+  </section>
+  <section class="c-section">
+    <h2 class="c-title">蓄電池があれば災害時にも非常に役立つ！</h2>
+    <p>日本は災害大国です。地震、台風による被災や停電は誰にでも身近に起こり得ます。</p>
+    <p>そんな時に太陽光パネルと蓄電池は非常に役立ちます。</p>
+    <p>戸建ての方だけではなくマンション住まいの方まで導入ができます。</p>
+    <p>またオール電化のマンション増えてます。蓄電器があれば停電時も安心です。</p>
+  </section>
+  <section class="c-section">
+    <h2 class="c-title">莫大な初期投資を抑え素早く節電</h2>
+    <p>蓄電器や太陽光パネルといった節電グッズの導入は初期費用が大きくかかることが多いです。</p>
+    <p>しかし、私は初期費用を大きく抑え、しかもたったの2年で減価償却することができました！</p>
+    <p>あなたもわたしと同じ方法で高騰する電気代を削減し、これから起こりうる災害に備えてみませんか？</p>
+    <div>
+      <a href="/" class="c-button w-full block text-center">電気代を削減するグッズはこちら</a>
+    </div>
+  </section>
 <?php get_footer(); ?>
